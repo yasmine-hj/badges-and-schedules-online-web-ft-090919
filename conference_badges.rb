ebf5da72ec38fec1f8bc551847f6f472e87d5b65
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-11 11:54:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,5 +17,9 @@ def assign_rooms(attendees)
 attendees.each_with_index.map do |attendee, index|
 "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
 end
+end
+
+def printer
+  batch_badge_creator
   
 end
